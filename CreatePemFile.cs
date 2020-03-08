8af722576017d90c6d78194294e408bbe4eed0fc
celadaris <imnotkira13r@gmail.com>
2020-03-07 20:28:21 -0600

    Update CreatePemFile.cs

--- CreatePemFile.cs
+++ CreatePemFile.cs
@@ -3,12 +3,7 @@ using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.OpenSsl;
 using Org.BouncyCastle.Security;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CreatePemFiles
 {
